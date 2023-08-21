58a06998156a3446e8b9cc851dccd9435b05c78c
Lualmara-5 <luimartinezra@unal.edu.co>
2023-08-21 11:58:00 -0500

    Class Auto Part 1

--- main.py
+++ main.py
@@ -10,4 +10,14 @@ class Asiento:
             self.color = color
 
 class Auto:
+    cantidadCreados = 0
+
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+
 class Motor:
