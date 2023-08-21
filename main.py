fa62fc26be2672e84ba739a209aadebb87d5c645
Lualmara-5 <luimartinezra@unal.edu.co>
2023-08-21 11:44:36 -0500

    Class Asiento Part 1

---
+++ main.py
@@ -0,0 +1,9 @@
+class Asiento:
+
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+
+class Auto:
+class Motor:
